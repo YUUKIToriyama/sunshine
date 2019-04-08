adb24d906f4d1eed1427ef50a6360c600fe40542
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-04-08 09:05:32 +0900

    Add files via upload
    map メソッドを使って書き直しました。十何byteか短くなりました。

--- ruby_script/checkdigit_ISBN13.rb
+++ ruby_script/checkdigit_ISBN13.rb
@@ -5,19 +5,19 @@
 
 isbn = "978410109205"
 
-arr_isbn = isbn.split("")
+arr = isbn.split("").map {|n| n.to_i}
 
 sum = 0
 # 奇数番号の数字には3を、偶数番号の数字には1をそれぞれ掛けます。
 for i in 1..isbn.length
 	if (i % 2 == 0)
-		arr_isbn[i-1] = arr_isbn[i-1].to_i * 3
+		arr[i-1] = arr[i-1] * 3
 	else
-		arr_isbn[i-1] = arr_isbn[i-1].to_i * 1
+		arr[i-1] = arr[i-1] * 1
 	end
-	sum = sum + arr_isbn[i-1]
+	sum = sum + arr[i-1]
 end
 
 checkdigit = 10 - (sum % 10)
 
-puts "出力: " + isbn + "-" + checkdigit.to_s
+puts "出力: #{isbn} - #{checkdigit}"
