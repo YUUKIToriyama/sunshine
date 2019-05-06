b65ccfd27eba4bbdac4b0523c291832a16a5b09e
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-05-06 13:09:49 +0900

    Update random_img.sh

--- shell_gei/random_img.sh
+++ shell_gei/random_img.sh
@@ -1,2 +1,8 @@
-echo "# ImageMagick pixel enumeration: 5,5,256,srgb"
-echo {0..4},{0..4} | tr " " "\n" | awk -F "," '{print $1,$2, int(rand()*256), int(rand()*256), int(rand()*256)}' | xargs printf "%d,%d: (%d,%d,%d)\n"
+# Usage
+# ./random_img.sh | convert "-" output.png
+
+width=100
+height=100
+
+echo "# ImageMagick pixel enumeration: $width,$height,256,srgb"
+echo {0..$(($width - 1))},{0..$(($height - 1))} | tr " " "\n" | awk -F "," '{print $1,$2, int(rand()*256), int(rand()*256), int(rand()*256)}' | xargs printf "%d,%d: (%d,%d,%d)\n"
