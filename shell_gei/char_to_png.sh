f541804a70a000163d7a160da922041eda28b402
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-07-08 16:46:53 +0900

    some fixes

--- shell_gei/char_to_png.sh
+++ shell_gei/char_to_png.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # convertコマンドを使って任意の文字を画像ファイルに変換するスクリプト
 
-mkdir images
-echo "先輩参戦！！！" | sed 's/./&\n/g' | sed '$d' \
-	| xargs -I@ convert -font IPA明朝 -size 1000x1000 -background Black -fill White -gravity Center -pointsize 500 -rotate -5 label:"@" images/@.png
+convert -font "IPA-P明朝" -size 500x500 -background Black -fill White -gravity Center -pointsize 500 $(echo "先輩参戦！！" | grep -o . | awk '{printf "label:\"%s\" ", $1}') -monitor images.png
--- shell_gei/seq_to_emoji.sh
+++ shell_gei/seq_to_emoji.sh
@@ -1,3 +1,3 @@
-seq -s" " 0x1F345 0x1F353 | xargs -I@ ruby -e '%w(@).each do |moji|; print moji.to_i.chr("UTF-8"); end'
+seq -s" " 0x1F345 0x1F353 | xargs -I@ ruby -e '%w(@).each{|m| print m.to_i.chr("UTF-8")}'
 
-#🍅🍆🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓
+# 🍅🍆🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓
